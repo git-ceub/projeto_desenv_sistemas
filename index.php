59e447ce0c77c21d70388c916e0c7be5bbb5c504
Francisco Carlos Molina Duarte Junior <francisco.carlos@dinfor.uniceub>
2026-03-17 20:45:24 -0300

    adicionando o script de banco

--- index.php
+++ index.php
@@ -1,16 +1,6 @@
 <?php
-    $host = "127.0.0.1";
-    $user = "root";
-    $porta = "3306";
-    $password = "ceub123456";
-    $db = "projeto";
 
-    $conexao = new PDO(
-        'mysql:host='.$host.';
-        port='.$porta.';
-        dbname='.$db,
-        $user,
-        $password);
+    include 'conexao.php';
 
     $sql = " SELECT * FROM aluno ";
     $consulta = $conexao->query($sql);
